d3690493d02c9f7adcc7bde7fb07fb6396f2ffad
pap0gallo <khodunov14@gmail.com>
2025-08-11 22:57:41 +0300

    solution for lesson 22

---
+++ public/L22/index.php
@@ -0,0 +1,81 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new Slim\Views\PhpRenderer(__DIR__ . '/templates');
+});
+$container->set('flash', function () {
+    return new Slim\Flash\Messages();
+});
+
+$app = AppFactory::createFromContainer($container);
+$app->add(MethodOverrideMiddleware::class);
+$app->addErrorMiddleware(true, true, true);
+
+$repo = new App\L22\src\PostRepository();
+
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');
+});
+
+$app->get('/posts', function ($request, $response) use ($repo) {
+    $flash = $this->get('flash')->getMessages();
+
+    $params = [
+        'flash' => $flash,
+        'posts' => $repo->all()
+    ];
+    return $this->get('renderer')->render($response, 'posts/index.phtml', $params);
+})->setName('posts');
+
+$app->get('/posts/new', function ($request, $response) use ($repo) {
+    $params = [
+        'postData' => [],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, 'posts/new.phtml', $params);
+});
+
+$app->post('/posts', function ($request, $response) use ($repo, $router) {
+    $postData = $request->getParsedBodyParam('post');
+
+    $validator = new App\L22\src\Validator();
+    $errors = $validator->validate($postData);
+
+    if (count($errors) === 0) {
+        $id = $repo->save($postData);
+        $this->get('flash')->addMessage('success', 'Post has been created');
+        return $response->withHeader('X-ID', $id)
+            ->withRedirect($router->urlFor('posts'));
+    }
+
+    $params = [
+        'postData' => $postData,
+        'errors' => $errors
+    ];
+
+    return $this->get('renderer')->render($response->withStatus(422), 'posts/new.phtml', $params);
+});
+
+// BEGIN (write your solution here)
+$app->delete('/posts/{id}', function ($request, $response, $args) use ($container, $repo, $router) {
+    $id = (string) $args['id'];
+    $postData = $repo->find($id);
+    if (!empty($postData)) {
+        $repo->destroy($id);
+        $container->get('flash')->addMessage('success', 'Post has been removed');
+        return $response->withRedirect($router->urlFor('posts'));
+    }
+    return $response->withStatus(404);
+});
+// END
+
+$app->run();
