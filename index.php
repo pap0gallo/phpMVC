ca26f04011fd915d5a6128d005f89c3d52b5a01d
pap0gallo <khodunov14@gmail.com>
2025-08-13 16:07:50 +0300

    solution for exercise 25

--- index.php
+++ index.php
@@ -12,20 +12,21 @@ session_start();
 
 //$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 
-$storage = function () {
-    $dir = __DIR__ . '/files';
-    $path = $dir . '/users_dp';
-    if (!is_dir($dir)) {
-        mkdir($dir, 0777, true);
-    }
-    if (!file_exists($path)) {
-        $data = [];
-    } else {
-        $content = file_get_contents($path);
-        $data = json_decode($content, true) ?? [];
-    }
-    return ['data' => $data, 'path' => $path];
-};
+//$storage = function () {
+//    $dir = __DIR__ . '/files';
+//    $path = $dir . '/users_dp';
+//    if (!is_dir($dir)) {
+//        mkdir($dir, 0777, true);
+//    }
+//    if (!file_exists($path)) {
+//        $data = [];
+//    } else {
+//        $content = file_get_contents($path);
+//        $data = json_decode($content, true) ?? [];
+//    }
+//    return ['data' => $data, 'path' => $path];
+//};
+
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -50,18 +51,17 @@ $app->get('/', function ($request, $response) {
     // return $response->write('Welcome to Slim!');
 })->setName('home');
 
-$app->get('/users', function ($request, $response) use ($storage) {
-    $repo = $storage();
+$app->get('/users', function ($request, $response) {
     $router = $this->get('router');
-    $users = $repo['data'];
+    $data = json_decode($request->getCookieParam('users', json_encode([])), true);
     $term = $request->getQueryParam('term') ?? '';
     $messages = $this->get('flash')->getMessages();
     if ($term) {
-        $params['users'] = collect($users)
+        $params['users'] = collect($data)
             ->filter(fn($item) => str_contains($item['nickname'], $term))
             ->all();
     } else {
-        $params['users'] = $users;
+        $params['users'] = $data;
     }
     $params['term'] = $term;
     $params['router'] = $router;
@@ -70,15 +70,6 @@ $app->get('/users', function ($request, $response) use ($storage) {
         ->get('renderer')
         ->render($response, 'users/index.phtml', $params);
 })->setName('users');
-
-//$app->post('/users', function ($request, $response) {
-//    return $response->withStatus(302);
-//});
-
-//$app->get('/courses/{id}', function ($request, $response, array $args) {
-//    $id = $args['id'];
-//    return $response->write("Course id: {$id}");
-//})->setName('course.show');
 
 $app->get('/users/new', function($request, $response) {
     $router = $this->get('router');
@@ -90,38 +81,35 @@ $app->get('/users/new', function($request, $response) {
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 })->setName('users.new');
 
-$app->post('/users', function($request, $response) use ($storage) {
-    $repo = $storage();
+$app->post('/users', function($request, $response) {
+    $data = json_decode($request->getCookieParam('users', json_encode([])), true);
     $router = $this->get('router');
     $validator = new Validator();
     $user = $request->getParsedBodyParam('user');
     $errors = $validator->validate($user);
     if (count($errors) === 0) {
-        $path = $repo['path'];
-        $data = $repo['data'];
         if (last($data)) {
             $user['id'] = last($data)['id'] + 1;
         } else {
             $user['id'] = 1;
         }
-        //$user['id'] = last($data)['id'] + 1 ?? 1;
-        //$data[array_key_last($data) + 1 ?? 1] = $user;
         $data [] = $user;
-        $json = json_encode($data, JSON_PRETTY_PRINT);
-        file_put_contents($path, $json);
+        $json = json_encode($data);
         $this->get('flash')->addMessage('success', 'User was added successfully');
-        return $response->withRedirect($router->urlFor('users'), 302);
+        return $response
+            ->withHeader('Set-Cookie', 'users=' . rawurlencode($json) . '; Path=/; HttpOnly')
+            ->withRedirect($router->urlFor('users'), 302);
     }
     $params = ['user' => $user, 'errors' => $errors, 'router' => $router];
     $response =  $response->withStatus(422);
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 })->setName('users.store');
 
-$app->get('/users/{id}', function ($request, $response, $args) use ($storage) {
-    $repo = $storage();
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $data = json_decode($request->getCookieParam('users', json_encode([])), true);
     $id = $args['id'];
     $router = $this->get('router');
-    $collection = collect($repo['data']);
+    $collection = collect($data);
     $user = $collection->firstWhere('id', (string) $id);
     if (empty($user)) {
         return $response->withStatus(404);
@@ -130,10 +118,10 @@ $app->get('/users/{id}', function ($request, $response, $args) use ($storage) {
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 })->setName('users.id');
 
-$app->get('/users/{id}/edit', function ($request, $response, $args) use ($container, $storage) {
-    $repo = $storage();
+$app->get('/users/{id}/edit', function ($request, $response, $args) use ($container) {
+    $data = json_decode($request->getCookieParam('users', json_encode([])), true);
     $id = (string) $args['id'];
-    $collection = collect($repo['data']);
+    $collection = collect($data);
     $user = $collection->firstWhere('id', $id);
     if (!$user) {
         return $response->withStatus(404);
@@ -147,14 +135,13 @@ $app->get('/users/{id}/edit', function ($request, $response, $args) use ($contai
     ]);
 })->setName('users.edit');
 
-$app->patch('/users/{id}', function ($request, $response, $args) use ($container, $storage) {
-    $repo = $storage();
-    $path = $repo['path'];
+$app->patch('/users/{id}', function ($request, $response, $args) use ($container) {
+    $data = json_decode($request->getCookieParam('users', json_encode([])), true);
     $idToUpdate = (string) $args['id'];
     $newUserData = $request->getParsedBodyParam('user');
     $validator = new Validator();
     $errors = $validator->validate($newUserData);
-    $collection = collect($repo['data']);
+    $collection = collect($data);
 
     // Проверка существования пользователя
     $userExists = $collection->contains(fn($item) => (string)$item['id'] === $idToUpdate);
@@ -173,11 +160,11 @@ $app->patch('/users/{id}', function ($request, $response, $args) use ($container
             }
             return $item;
         });
-
-        file_put_contents($path, json_encode($updated->all(), JSON_PRETTY_PRINT));
-
+        $json = json_encode($updated);
         $container->get('flash')->addMessage('success', 'User was edited successfully');
-        return $response->withRedirect(
+        return $response
+            ->withHeader('Set-Cookie', 'users=' . rawurlencode($json) . '; Path=/; HttpOnly')
+            ->withRedirect(
             $container->get('router')->urlFor('users.edit', ['id' => $idToUpdate])
         );
     }
@@ -191,11 +178,10 @@ $app->patch('/users/{id}', function ($request, $response, $args) use ($container
     ]);
 });
 
-$app->delete('/users/{id}', function ($request, $response, $args) use ($container, $storage) {
-    $repo = $storage();
+$app->delete('/users/{id}', function ($request, $response, $args) use ($container) {
+    $data = json_decode($request->getCookieParam('users', json_encode([])), true);
     $id = (string) $args['id'];
-    $path = $repo['path'];
-    $collection = collect($repo['data']);
+    $collection = collect($data);
     $user = $collection->firstWhere('id', $id);
     if (!$user) {
         return $response->withStatus(404);
@@ -203,9 +189,11 @@ $app->delete('/users/{id}', function ($request, $response, $args) use ($containe
     $updated = $collection->reject(function ($item) use ($id) {
         return (string) $item['id'] === $id;
     })->all();
-    file_put_contents($path, json_encode($updated, JSON_PRETTY_PRINT));
+    $json = json_encode($updated);
     $container->get('flash')->addMessage('success', 'User was deleted successfully');
-    return $response->withRedirect(
+    return $response
+        ->withHeader('Set-Cookie', 'users=' . rawurlencode($json) . '; Path=/; HttpOnly')
+        ->withRedirect(
         $container->get('router')->urlFor('users.store')
     );
 });
