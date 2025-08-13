8fafaaa33012b36d6b5163a4ab4efe103437674d
pap0gallo <khodunov14@gmail.com>
2025-08-13 13:01:45 +0300

    solution for lesson 24

--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 start:
-	php -S localhost:8080 -t public public/L22/index.php
+	php -S localhost:8080 -t public public/L24/index.php
 	#php -S localhost:8080 -t public index.php
---
+++ public/L24/src/Validator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\L24\src;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+class Validator
+{
+    public function validate(array $course)
+    {
+        $errors = [];
+        if ($course['name'] === '') {
+            $errors['name'] = "Can't be blank";
+        }
+
+        if (empty($course['body'])) {
+            $errors['body'] = "Can't be blank";
+        }
+
+        return $errors;
+    }
+}
